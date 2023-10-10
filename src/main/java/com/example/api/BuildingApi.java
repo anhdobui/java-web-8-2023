e1fee8012872d2f4cc232f872b2c5087338f170e
anhdobui <anhdobui2002@gmail.com>
2023-10-10 14:55:53 +0700

    demo PropertySource

--- src/main/java/com/example/api/BuildingApi.java
+++ src/main/java/com/example/api/BuildingApi.java
@@ -1,15 +1,9 @@
 package com.example.api;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,9 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.customexception.FieldRequiredException;
@@ -33,6 +25,7 @@ import com.example.service.BuildingService;
 public class BuildingApi {
 	@Autowired 
 	private BuildingService buildingService;
+	
 	
 	@GetMapping
 	public  List<BuildingSearchResponse> findAll(@RequestParam( name = "name",required = false) String name,@RequestParam(name = "numberofbasement" ,required = false) Integer numberofbasement) {
--- src/main/java/com/example/repository/impl/BuildingRepositoryImpl.java
+++ src/main/java/com/example/repository/impl/BuildingRepositoryImpl.java
@@ -8,23 +8,34 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Repository;
 
 import com.example.repository.BuildingRepository;
 import com.example.repository.entity.BuildingEntity;
 
 @Repository
+@PropertySource("classpath:application.properties")
 public class BuildingRepositoryImpl implements BuildingRepository {
-	private final String DB_URL = "jdbc:mysql://localhost:3306/estatebasic";
-	private final String USER = "root";
-	private final String PASS = "root";
+	/*
+	 * private final String DB_URL = "jdbc:mysql://localhost:3306/estatebasic";
+	 * private final String USER = "root";
+	 * private final String PASS = "root";
+	 */
 	
+	@Value("${spring.datasource.url}")
+	private String dbUrl;
+	@Value("${spring.datasource.username}")
+	private String user;
+	@Value("${spring.datasource.password}")
+	private String password;
 	
 	@Override
 	public List<BuildingEntity> findAll() {
 		List<BuildingEntity> results = new ArrayList<>();
 		String QUERY = "SELECT * FROM building";
-		try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+		try(Connection conn = DriverManager.getConnection(dbUrl, user, password);
 		         Statement stmt = conn.createStatement();
 		         ResultSet rs = stmt.executeQuery(QUERY);
 		      ) {		      
