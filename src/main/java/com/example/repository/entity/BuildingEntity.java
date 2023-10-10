17ee609c16db1471514fdd67797d19df2abb99f7
anhdobui <anhdobui2002@gmail.com>
2023-10-10 18:47:37 +0700

    onetomany and manytoone

--- src/main/java/com/example/repository/entity/BuildingEntity.java
+++ src/main/java/com/example/repository/entity/BuildingEntity.java
@@ -1,10 +1,44 @@
 package com.example.repository.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "building")
 public class BuildingEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@OneToMany(mappedBy = "building")
+	private List<RentAreaEntity> rentAreas = new ArrayList<>();
+	
+	@Column
 	private String street;
+	
+	@Column
 	private String ward;
+	
+	@Column(name = "districtid")
 	private Long districtId;
+	
+	@Column
 	private String name;
+	
+	public List<RentAreaEntity> getRentAreas() {
+		return rentAreas;
+	}
+	public void setRentAreas(List<RentAreaEntity> rentAreas) {
+		this.rentAreas = rentAreas;
+	}
 	public String getStreet() {
 		return street;
 	}
@@ -29,5 +63,12 @@ public class BuildingEntity {
 	public void setName(String name) {
 		this.name = name;
 	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
 	
 }
